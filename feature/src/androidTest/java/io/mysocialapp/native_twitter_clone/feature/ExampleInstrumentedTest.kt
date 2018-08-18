cf50fe6a381829d35014d525382d1e309c0f8ec0
Romaric Philogene <evoxmusic@gmail.com>
2018-08-18 13:17:27 +0200

    initial commit

---
+++ feature/src/androidTest/java/io/mysocialapp/native_twitter_clone/feature/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package io.mysocialapp.native_twitter_clone.feature
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("io.mysocialapp.native_twitter_clone.feature.test", appContext.packageName)
+    }
+}
